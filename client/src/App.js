5bfe5bd3f0602f5a918b5912d074ab34c7ac8ebb
Greg Peranich <gregperanich@gregs-mbp.attlocal.net>
2019-06-28 14:54:07 -0400

    added materialize css

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ class App extends React.Component {
             showEmptyRes: true
           })
         }
-        
+
       })
       .catch((err) => {
         console.log('err', err);
@@ -39,26 +39,28 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>Weather App</h1>
-        <select defaultValue="" onChange={(e) => this.handleChange(e.target.value)}>
-          <option value="" disabled>Select your weather condition...</option>
-          {weatherOptions.map((option) => {
-            return (
-              <option value={option} key={option}>{option}</option>
-            )
-          })}
-        </select>
-          {this.state.showCities && (
-            <div>
-              {Object.keys(this.state.cityList).map((key, index) => {
-                return (
-                  <p key={index}>{key}, {this.state.cityList[key].state} Temperature {this.state.cityList[key].temperature} Wind Speed {this.state.cityList[key].wind}</p>
-                )
-              })}
-            </div>
-          )}
-          {this.state.showEmptyRes && (
-            <p>No cities are experiencing this weather condition!</p>
-          )}
+        <div className="input-field" style={{textAlign: 'middle', margin: 'auto'}}>
+          <select style={{display: 'block', width: '50%', margin: 'auto'}} defaultValue="" onChange={(e) => this.handleChange(e.target.value)}>
+            <option value="" disabled>Select your weather condition...</option>
+            {weatherOptions.map((option) => {
+              return (
+                <option value={option} key={option}>{option}</option>
+              )
+            })}
+          </select>
+        </div>
+        {this.state.showCities && (
+          <ul className='collection' style={{width: '60%', margin: 'auto', marginTop: 25, marginBottom: 25}}>
+            {Object.keys(this.state.cityList).map((key, index) => {
+              return (
+                <li className='collection-item' key={index}>{key}, {this.state.cityList[key].state} Temperature {this.state.cityList[key].temperature} Wind Speed {this.state.cityList[key].wind}</li>
+              )
+            })}
+          </ul>
+        )}
+        {this.state.showEmptyRes && (
+          <p>No cities are experiencing this weather condition!</p>
+        )}
       </div>
     );
   }
